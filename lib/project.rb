ba35a96e0003ad261252b83d9a72a3109c5828c2
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-09 21:51:26 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
 
   def add_backer(backer)
     @backers.push(backer)
-    backer.back_project(self) unless backer.backed_project.include?(self)
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 end
